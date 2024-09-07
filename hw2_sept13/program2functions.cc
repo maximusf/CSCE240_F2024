a76c6ab4eb339a4ed193d5cf5a89cd386c108b1d
Maximus Fernandez <maximusfernandez98@gmail.com>
2024-09-07 12:30:24 -0400

    Added hw2

---
+++ hw2_sept13/program2functions.cc
@@ -0,0 +1,8 @@
+// Copyright 2024 maximusf
+// Due: 2024-09-13
+// program2functions.cc
+// program 2 functions
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "program2functions.h"
---
+++ hw2_sept13/program2functions.h
@@ -0,0 +1,12 @@
+// Copyright 2024 maximusf
+// Due: 2024-09-13
+// program2functions.h
+// program 2 for csce240
+#ifndef PROGRAM2FUNCTIONS_H
+#define PROGRAM2FUNCTIONS_H
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#endif
