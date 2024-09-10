c81b85ef7f977e0f611a8464cb764f139843646d
Maximus Fernandez <maximusfernandez98@gmail.com>
2024-09-09 23:50:55 -0400

    reupload 1

--- hw2_sept13/program2.cc
+++ hw2_sept13/program2.cc
@@ -1,9 +1,9 @@
 // Copyright 2024 maximusf
 // Due: 2024-09-13
 // program2.cc
-// program 2 for csce240
+// program 2 driver file
 #include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
-#include "program2functions.h"
+#include "./program2functions.h"
--- hw2_sept13/program2functions.h
+++ hw2_sept13/program2functions.h
@@ -1,12 +1,22 @@
 // Copyright 2024 maximusf
 // Due: 2024-09-13
 // program2functions.h
-// program 2 for csce240
-#ifndef PROGRAM2FUNCTIONS_H
-#define PROGRAM2FUNCTIONS_H
+// program 2 header file
+#ifndef _PROGRAM2FUNCTIONS_H_
+#define _PROGRAM2FUNCTIONS_H_
 #include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
+
+bool LeapYear(int);
+
+int LastDayOfMonth(int, int = 0);
+
+bool ValidDate(int, int, int);
+
+void NextDate(int&, int&, int&);
+
+void PreviousDate(int&, int&, int&);
 
 #endif
